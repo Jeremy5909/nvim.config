3c9ab35cace553456fd571db5f2117f4c451782b
Jeremy5909 <jeremyherczeg@gmail.com>
2025-03-29 19:03:20 -0700

    Formatting

--- lua/jeremy/core/keymaps.lua
+++ lua/jeremy/core/keymaps.lua
@@ -2,22 +2,22 @@ vim.g.mapleader = " "
 local k = vim.keymap
 
 -- Move highlighted text
-k.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move highlighted text up"})
-k.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move highlighted text down"})
+k.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighted text up" })
+k.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move highlighted text down" })
 
 -- Better combining
-k.set("n", "J", "mzJ`z", {desc = "Better combining"})
+k.set("n", "J", "mzJ`z", { desc = "Better combining" })
 
 -- Better full page jumping
-k.set("n", "<C-d>", "<C-d>zz", {desc = "better full page jumping up"})
-k.set("n", "<C-u>", "<C-u>zz", {desc = "better full page jumping down"})
+k.set("n", "<C-d>", "<C-d>zz", { desc = "Better full page jumping up" })
+k.set("n", "<C-u>", "<C-u>zz", { desc = "Better full page jumping down" })
 
 -- Better searching
-k.set("n", "n", "nzzzv", {desc = "better searching up"})
-k.set("n", "N", "Nzzzv", {desc = "better searching down"})
+k.set("n", "n", "nzzzv", { desc = "Better searching up" })
+k.set("n", "N", "Nzzzv", { desc = "Better searching down" })
 
 -- Unbind evil Q
-vim.keymap.set("n", "Q", "<nop>", {desc = "unbind evil q"})
+vim.keymap.set("n", "Q", "<nop>", { desc = "unbind evil q" })
 
 -- Window management
 k.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
