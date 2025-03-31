1132a80f0bdff79e4449f325918e4082aca2a9a7
Jeremy5909 <jeremyherczeg@gmail.com>
2025-03-31 13:16:43 -0700

    Hopefully fix auto-session auto saving and creating

--- lua/jeremy/plugins/auto-session.lua
+++ lua/jeremy/plugins/auto-session.lua
@@ -1,10 +1,16 @@
 return {
 	"rmagatti/auto-session",
 	lazy = false,
-
+	keys = {
+		{ "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+		{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+		{ "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+	},
 	---@modules "auto-session"
 	---@type AutoSession.Config
 	opts = {
+		auto_save = true,
+		auto_create = true,
 		lazy_support = true,
 	},
 }
