4d5d6c00806aba02b6fe068a73cb5c99448a7f8a
Jeremy5909 <jeremyherczeg@gmail.com>
2025-03-31 16:55:34 -0700

    Celluar automata

---
+++ lua/jeremy/plugins/celluar-automaton.lua
@@ -0,0 +1,8 @@
+return {
+	"eandrju/cellular-automaton.nvim",
+	keys = {
+		{ "<leader>uu", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
+		{ "<leader>ui", "<cmd>CellularAutomaton game_of_life<CR>", desc = "Game of life" },
+		{ "<leader>uo", "<cmd>CellularAutomaton scramble<CR>", desc = "Scramble" },
+	},
+}
