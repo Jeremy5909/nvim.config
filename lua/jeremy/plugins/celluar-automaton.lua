4e4686d52948da7205b353dccbf3412b78e5ead4
Jeremy5909 <jeremyherczeg@gmail.com>
2025-03-31 21:50:24 -0700

    Make cellular automata lazier

--- lua/jeremy/plugins/celluar-automaton.lua
+++ lua/jeremy/plugins/celluar-automaton.lua
@@ -1,5 +1,6 @@
 return {
 	"eandrju/cellular-automaton.nvim",
+	event = "VeryLazy",
 	keys = {
 		{ "<leader>uu", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
 		{ "<leader>ui", "<cmd>CellularAutomaton game_of_life<CR>", desc = "Game of life" },
