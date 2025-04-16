686dfa5e2a136ad5bb86aa6f8867101357c246d2
Jeremy5909 <jeremyherczeg@gmail.com>
2025-04-15 19:47:33 -0700

    Fix image.nvim

--- lua/jeremy/plugins/image.lua
+++ lua/jeremy/plugins/image.lua
@@ -1,1 +1,13 @@
-return { "3rd/image.nvim" }
+return {
+	"3rd/image.nvim",
+	dependencies = {
+		{
+			"vhyrro/luarocks.nvim",
+			priority = 1001, -- this plugin needs to run before anything else
+			opts = {
+				rocks = { "magick" },
+			},
+		},
+	},
+	opts = {},
+}
