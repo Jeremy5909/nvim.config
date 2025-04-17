a07590e303b31277d2ba85b45b27c53d90305646
Jeremy5909 <jeremyherczeg@gmail.com>
2025-04-17 00:32:39 -0700

    More molten keybinds

--- lua/jeremy/plugins/molten.lua
+++ lua/jeremy/plugins/molten.lua
@@ -7,7 +7,7 @@ return {
 		vim.g.python3_host_prog = vim.fn.expand("~/.venv/neovim/bin/python3")
 
 		-- Automatically launch correct kernel
-		vim.keymap.set("n", "<leader>ip", function()
+		vim.keymap.set({ "n", "v" }, "<leader>mi", function()
 			local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
 			if venv ~= nil then
 				-- in the form of /home/benlubas/.virtualenvs/VENV_NAME
@@ -17,5 +17,16 @@ return {
 				vim.cmd("MoltenInit python3")
 			end
 		end, { desc = "Initialize Molten for python3", silent = true })
+
+		vim.keymap.set("n", "<leader>ml", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Evaluate line" })
+		vim.keymap.set("n", "<leader>mm", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Re-evaluate cell" })
+		vim.keymap.set(
+			"v",
+			"<leader>mm",
+			":<C-u>MoltenEvaluateVisual<CR>",
+			{ silent = true, desc = "evaluate visual selection" }
+		)
+		vim.keymap.set("n", "<leader>mx", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
+		vim.keymap.set("n", "<leader>mh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
 	end,
 }
