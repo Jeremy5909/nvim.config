1c0484084877805ee4ad5d1b32eb0d09246c133e
Jeremy5909 <jeremyherczeg@gmail.com>
2025-06-09 12:33:54 -0700

    fugitive keybinds

---
+++ lua/jeremy/plugins/git.lua
@@ -0,0 +1,10 @@
+return {
+	"tpope/vim-fugitive",
+	keys = {
+		{ "<leader>gw", "<cmd>Gw<CR>", desc = "git add {current file}" },
+		{ "<leader>gs", "<cmd>G status<CR>", desc = "git status" },
+		{ "<leader>ga", "<cmd>G add .<CR>", desc = "git add ." },
+		{ "<leader>gp", "<cmd>G push<CR>", desc = "git push" },
+		{ "<leader>gc", "<cmd>G commit<CR>", desc = "git commit" },
+	},
+}
--- lua/jeremy/plugins/init.lua
+++ lua/jeremy/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim",
 	"christoomey/vim-tmux-navigator",
-	"tpope/vim-fugitive",
 	"vuciv/golf",
 	{ "3rd/image.nvim", opts = { processor = "magick_rock" } },
 	{ "echasnovski/mini.ai", version = false, config = true },
