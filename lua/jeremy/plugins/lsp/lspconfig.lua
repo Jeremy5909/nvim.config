f56c6c8875a0222fb2e461e9b4e301899fe3f98a
Jeremy5909 <jeremyherczeg@gmail.com>
2025-10-28 09:37:40 -0700

    clippy for rust correctly and inlay hints enabled

--- lua/jeremy/plugins/lsp/lspconfig.lua
+++ lua/jeremy/plugins/lsp/lspconfig.lua
@@ -5,14 +5,21 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
+		vim.lsp.inlay_hint.enable(true)
+
 		vim.lsp.config("lua_ls", {
 			settings = {
 				Lua = {
 					diagnostics = {
-						globals = {
-							"vim",
-						},
+						globals = { "vim" },
 					},
+				},
+			},
+		})
+		vim.lsp.config("rust_analyzer", {
+			settings = {
+				["rust-analyzer"] = {
+					check = { command = "clippy" },
 				},
 			},
 		})
