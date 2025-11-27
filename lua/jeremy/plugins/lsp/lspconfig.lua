b099ab1650bdd66070f6cb89eadcf9fd0029030a
Jeremy5909 <jeremyherczeg@gmail.com>
2025-11-26 20:51:47 -0700

    nvim-jdtls

--- lua/jeremy/plugins/lsp/lspconfig.lua
+++ lua/jeremy/plugins/lsp/lspconfig.lua
@@ -2,9 +2,12 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
+		"mfussenegger/nvim-jdtls",
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
+		vim.lsp.enable("jdtls")
+
 		vim.lsp.inlay_hint.enable(true)
 
 		vim.lsp.config("lua_ls", {
